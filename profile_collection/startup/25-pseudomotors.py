5ddc280d6741ed5e20316c94fde6480f18911c57
Thomas A Caswell <tcaswell@bnl.gov>
2019-02-07 09:04:06 -0500

    MNT: whitespace

--- profile_collection/startup/25-pseudomotors.py
+++ profile_collection/startup/25-pseudomotors.py
@@ -5,63 +5,17 @@ import numpy as np
 from scipy import interpolate
 
 
-_ivu_gap = [ 6184.0,
-       6368.0,
-       6550.0,
-       6730.0,
-       6909.0,
-       7086.0,
-       7262.0,
-       7438.0,
-       7613.0,
-       7788.0,
-       7962.0,
-       8138.0,
-       8311.0,
-       8486.0,
-       8662.0,
-       8840.0,
-       9019.0,
-       9200.0,
-       9382.0,
-       9567.0,
-       9753.0,
-       9943.0,
-       10136.0,
-       10332.0,
-       10531.0,
-       10740.0,
-       10960.0,
-       11385.0]
+_ivu_gap = [6184.0, 6368.0, 6550.0, 6730.0, 6909.0, 7086.0, 7262.0,
+            7438.0, 7613.0, 7788.0, 7962.0, 8138.0, 8311.0, 8486.0,
+            8662.0, 8840.0, 9019.0, 9200.0, 9382.0, 9567.0, 9753.0,
+            9943.0, 10136.0, 10332.0, 10531.0, 10740.0, 10960.0,
+            11385.0]
 
-_Bragg = [ 16.401781,
-       15.821071,
-       15.280587,
-       14.776273,
-       14.304428,
-       13.862182,
-       13.446656,
-       13.055574,
-       12.686789,
-       12.338436,
-       12.008851,
-       11.696607,
-       11.400018,
-       11.118472,
-       10.850483,
-       10.595196,
-       10.351707,
-       10.119222,
-       9.896987,
-       9.684357,
-       9.480717,
-       9.28554,
-       9.098203,
-       8.918375,
-       8.7455,
-       8.579178,
-       8.41915,
-       8.11645 ]
+_Bragg = [16.401781, 15.821071, 15.280587, 14.776273, 14.304428,
+          13.862182, 13.446656, 13.055574, 12.686789, 12.338436,
+          12.008851, 11.696607, 11.400018, 11.118472, 10.850483,
+          10.595196, 10.351707, 10.119222, 9.896987, 9.684357, 9.480717,
+          9.28554, 9.098203, 8.918375, 8.7455, 8.579178, 8.41915, 8.11645]
 
 gcalc = interpolate.interp1d(_Bragg, _ivu_gap)
 
@@ -76,7 +30,6 @@ class BLEnergy(PseudoPositioner):
     theta = Cpt(EpicsMotor, 'XF:10IDA-OP{Mono:DCM-Ax:P}Mtr')
     z2 = Cpt(EpicsMotor, 'XF:10IDA-OP{Mono:DCM-Ax:Z2}Mtr')
     ugap = Cpt(Undulator, 'SR:C10-ID:G1{IVU22:1')
-
 
     def forward(self, pseudo_pos):
         _th = np.rad2deg(np.arcsin(_hc/(2.*_si_111*pseudo_pos)))
@@ -101,7 +54,6 @@ class DCMEnergy(PseudoPositioner):
     theta = Cpt(EpicsMotor, 'XF:10IDA-OP{Mono:DCM-Ax:P}Mtr')
     z2 = Cpt(EpicsMotor, 'XF:10IDA-OP{Mono:DCM-Ax:Z2}Mtr')
 
-
     def forward(self, pseudo_pos):
         _th = np.rad2deg(np.arcsin(_hc/(2.*_si_111*pseudo_pos)))
         _z2 = 15./np.cos(np.deg2rad(_th)) - 15.
@@ -120,6 +72,8 @@ dcmE = DCMEnergy('', name='dcmE', egu='eV')
 # constants for HRM2 energy conversion
 _TB = 69.2943
 _EB = 9.1317
+
+
 class HRMEnergy(PseudoPositioner):
     energy = Cpt(PseudoSingle)
 
@@ -162,7 +116,7 @@ hn = np.sqrt(h2)
 
 
 class AnalyzerCXtal(PseudoPositioner):
-    the  = Cpt(PseudoSingle, egu='deg')
+    the = Cpt(PseudoSingle, egu='deg')
     y = Cpt(PseudoSingle, egu='mm')
 
     uy = Cpt(EpicsMotor, 'XF:10IDD-OP{Analy:1-Ax:UY}Mtr')
