e1f1f11beb4223f1222767a6201cc3132cf795fe
Thomas A Caswell <tcaswell@bnl.gov>
2022-07-26 22:33:37 -0400

    STY: run black

--- startup/00-startup.py
+++ startup/00-startup.py
@@ -1,32 +1,35 @@
 from ophyd.signal import EpicsSignalBase
+
 EpicsSignalBase.set_defaults(timeout=10, connection_timeout=10)  # new style
 
 import nslsii
-nslsii.configure_base(get_ipython().user_ns, 'ixs')
+
+nslsii.configure_base(get_ipython().user_ns, "ixs")
 
 # After the above call, you will now have the following in your namespace:
 #
-#	RE : RunEngine
-#	db : databroker
-#	sd : SupplementalData
-#	pbar_manager : ProgressBarManager
-#	bec : BestEffortCallback
-#	peaks : bec.peaks
-#	plt : matplotlib.pyplot
-#	np : numpy
-#	bc : bluesky.callbacks
-#	bp : bluesky.plans
-#	bps : bluesky.plan_stubs
-#	mv : bluesky.plan_stubs.mv
-#	mvr : bluesky.plan_stubs.mvr
-#	mov : bluesky.plan_stubs.mov
-#	movr : bluesky.plan_stubs.movr
-#	bpp : bluesky.preprocessors
+# 	RE : RunEngine
+# 	db : databroker
+# 	sd : SupplementalData
+# 	pbar_manager : ProgressBarManager
+# 	bec : BestEffortCallback
+# 	peaks : bec.peaks
+# 	plt : matplotlib.pyplot
+# 	np : numpy
+# 	bc : bluesky.callbacks
+# 	bp : bluesky.plans
+# 	bps : bluesky.plan_stubs
+# 	mv : bluesky.plan_stubs.mv
+# 	mvr : bluesky.plan_stubs.mvr
+# 	mov : bluesky.plan_stubs.mov
+# 	movr : bluesky.plan_stubs.movr
+# 	bpp : bluesky.preprocessors
 
 
 # At the end of every run, verify that files were saved and
 # print a confirmation message.
 from bluesky.callbacks.broker import verify_files_saved
+
 # RE.subscribe(post_run(verify_files_saved), 'stop')
 
 
@@ -44,20 +47,23 @@ def relabel_fig(fig, new_label):
     fig.set_label(new_label)
     fig.canvas.manager.set_window_title(fig.get_label())
 
+
 # Implementing grid on plots:
 import matplotlib as mpl
-mpl.rcParams['axes.grid'] = True
 
-RE.md['beamline_id'] = 'IXS'
-RE.md['owner'] = 'xf10id'
-RE.md['group'] = 'ixs'
+mpl.rcParams["axes.grid"] = True
+
+RE.md["beamline_id"] = "IXS"
+RE.md["owner"] = "xf10id"
+RE.md["group"] = "ixs"
 
 from pyOlog.ophyd_tools import get_all_positioners
+
 
 def relabel_motors():
     mtrs = get_all_positioners()
     for mtr in mtrs:
-        attr = getattr(mtr, 'user_readback')
+        attr = getattr(mtr, "user_readback")
         attr.name = mtr.name
 
 # ## Live specfile exporting
