11b429bc76c5a5fdb576cd3692317049717a1c11
IXS operator <asuvorov@bnl.gov>
2024-07-01 14:30:37 -0400

    Added several functions to save, load, and preview ixs4c configuration file

--- startup/ixs4c.py
+++ startup/ixs4c.py
@@ -15,6 +15,27 @@ class FourCircle(SimMixin, E4CV):
     phi = Cpt(SoftPositioner, kind="hinted", init_pos=0)
     tth = Cpt(SoftPositioner, kind="hinted", init_pos=0)
 
+def save_config(file_name:str):
+# saves the ixs4c_config to a file
+    fname = file_name + '.json'
+    config_file = config_path / fname
+    ixs4c_config.export(config_file)
+    print(f"Configuration saved to {config_file}")
+
+def load_config(file_name:str):
+# loads the ixs4c_config from a file
+    fname = file_name + '.json'
+    config_file = config_path / fname
+    ixs4c_config.restore(config_file)
+    print(f"Configuration loaded from {config_file}")
+
+def preview_config(file_name:str):
+# previews the ixs4c_config from a file
+    fname = file_name + '.json'
+    config_file = config_path / fname
+    print(ixs4c_config.preview(config_file))
+
+
 ixs4c = FourCircle("", name="ixs4c")
 ixs4c.calc.physical_axis_names = {'omega': 'the', 'chi': 'chi', 'phi': 'phi', 'tth': 'tth'}
 ixs4c.engine.mode = "constant_phi"
@@ -23,12 +44,16 @@ ixs4c_config = DiffractometerConfiguration(ixs4c)
 
 config_path = pathlib.Path("/IXS2/data/Ixs4c_config")
 
-## aa a sample
+## add a sample:
 #ixs4c.calc.new_sample('MnF2', lattice=Lattice(a=4.873, b=4.873, c=3.13, alpha=90, beta=90, gamma=90))
 
-## add two, known reflections
+## add two, known reflections:
 # r1 = ixs4c.calc.sample.add_reflection(1,1,0,position=ixs4c.calc.Position(tth=22.7251, the=7.16641,chi=-4.43721,phi=0))
-# r2=ixs4c.calc.sample.add_reflection(2,1,0,position=ixs4c.calc.Position(tth=36.2993, the=31.8763,chi=1.2541,phi=0))
+# r2 = ixs4c.calc.sample.add_reflection(2,1,0,position=ixs4c.calc.Position(tth=36.2993, the=31.8763,chi=1.2541,phi=0))
+
+# add reflections from the current position:
+# r3 = ixs4c.calc.sample.add_reflection(1,1,1, ixs4c.real_position}
+# 
 
 ## compute orientation matrix UB
 # ixs4c.calc.sample.compute_UB(r1,r2)
@@ -46,6 +71,7 @@ config_path = pathlib.Path("/IXS2/data/Ixs4c_config")
 # ixs4c.position -> HKL related to the current physical position
 # ixs4c.real_position -> current physical position
 # ixs4c.calc.forward((1,0,1)) -> calculates physical motor positions
+
 # given an HKL
 # ixs4c.move(0,0,0) -> physical motion corresponding to a given HKL
 
